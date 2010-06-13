1b4c4657d0dabcab8fc53e264851f450e1824267
Thomas Hunger <tehunger@gmail.com>
2010-06-13 20:20:48 +0100

    Make generated make file depend on directories to detect deleted files

--- dependencies.py
+++ dependencies.py
@@ -8,15 +8,17 @@ except IndexError:
     directory = '.'
 
 interest = [
-    os.path.join(root, f)
+    (root, os.path.join(root, f))
     for root, dirs, files in os.walk(directory)
     for f in files
     if f.endswith('.scala')
 ]
 
-class_names = [(i, re.findall('class\s+(\w+)', open(i).read())) for i in interest]
+class_names = [(i, re.findall('class\s+(\w+)', open(i).read())) for _, i in interest]
 all_classes = ['%s.class' % n for _, names in class_names for n in names]
-print "CLASSES = %s" % " ".join(all_classes)
+print "CLASSES = %s ." % (" ".join(all_classes))
+print "gen/scalamake.mk: %s" % (" ".join(set(root for root, _ in  interest)))
+
 for path, names in class_names:
     print "%s: %s" % (" ".join("gen/$(PACKAGE_PATH)/%s.class" % n for n in names), path)
     print "\t	scalac -classpath $(CLASSPATH) $< -d gen"
